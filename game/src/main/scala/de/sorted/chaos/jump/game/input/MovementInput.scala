0acdee66fb62c0ea0226a9c2b6d3e6200a68a2c7
Sorted Chaos <the.sorted.chaos@gmail.com>
2020-11-17 18:26:14 +0100

    Fix overlapping key problem

--- game/src/main/scala/de/sorted/chaos/jump/game/input/MovementInput.scala
+++ game/src/main/scala/de/sorted/chaos/jump/game/input/MovementInput.scala
@@ -7,13 +7,15 @@ import org.lwjgl.glfw.GLFW
 object MovementInput {
 
   def processInput(windowId: Long, configuration: GameConfiguration): PlayerModification = {
-    val velocityX   = configuration.playerMovement.velocityX
+    val velocityX       = configuration.playerMovement.velocityX
     val playerModifiers = PlayerModification.init
-    var newVelocity = new Vector3f(0.0f, 0.0f, 0.0f)
+    var newVelocity     = new Vector3f(0.0f, 0.0f, 0.0f)
 
-    if (GLFW.glfwGetKey(windowId, GLFW.GLFW_KEY_A) == GLFW.GLFW_PRESS) {
+    if (GLFW.glfwGetKey(windowId, GLFW.GLFW_KEY_A) == GLFW.GLFW_PRESS &&
+        GLFW.glfwGetKey(windowId, GLFW.GLFW_KEY_D) != GLFW.GLFW_PRESS) {
       newVelocity = newVelocity.add(-velocityX, 0.0f, 0.0f)
-    } else if (GLFW.glfwGetKey(windowId, GLFW.GLFW_KEY_D) == GLFW.GLFW_PRESS) {
+    } else if (GLFW.glfwGetKey(windowId, GLFW.GLFW_KEY_D) == GLFW.GLFW_PRESS &&
+               GLFW.glfwGetKey(windowId, GLFW.GLFW_KEY_A) != GLFW.GLFW_PRESS) {
       newVelocity = newVelocity.add(velocityX, 0.0f, 0.0f)
     } else if (GLFW.glfwGetKey(windowId, GLFW.GLFW_KEY_W) == GLFW.GLFW_PRESS) {
       // rotation.x = 20.0f
