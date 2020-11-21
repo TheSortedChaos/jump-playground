1676350e1299bcbd1f2f1e277c0f080d49fd991f
Sorted Chaos <the.sorted.chaos@gmail.com>
2020-11-21 21:11:21 +0100

    Add initial simple GUI for Editor

---
+++ editor/src/main/scala/de/sorted/chaos/jump/editor/level/components/ExportButton.scala
@@ -0,0 +1,8 @@
+package de.sorted.chaos.jump.editor.level.components
+
+import javax.swing.JButton
+
+class ExportButton extends JButton {
+  this.setText("Export")
+
+}
---
+++ editor/src/main/scala/de/sorted/chaos/jump/editor/level/components/GridCheckbox.scala
@@ -0,0 +1,7 @@
+package de.sorted.chaos.jump.editor.level.components
+
+import javax.swing.JCheckBox
+
+class GridCheckbox extends JCheckBox {
+  this.setText("display Grid")
+}
